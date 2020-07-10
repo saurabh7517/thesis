cca63106e0d5a7a1c27ddcc62b287187758cedb0
saurabh <saurabh7517@gmail.com>
2020-07-10 14:29:03 +0100

    creating a nats connection for global access

--- main.go
+++ main.go
@@ -3,28 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/saurbh7517/artifact/connection"
 	"github.com/saurbh7517/artifact/controllers"
 )
 
 func main() {
 
+	connection.GetNatsConnection()
+
 	controllers.RegisterController()
 
 	http.ListenAndServe(":8080", nil)
-
-	// var clientset *kubernetes.Clientset = connection.CreateConnection()
-
-	// access the API to list pods
-	// pods, _ := clientset.CoreV1().Pods("").List(v1.ListOptions{})
-
-	// podfunction.CreateNewPod(clientset)
-
-	// deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-	// storageClient := clientset.StorageV1().RESTClient()
-	// request := storageClient.Post()
-
-	// request.Body()
-
-	// fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
 }
